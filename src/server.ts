56e7dd464c401e695648ef6f04f5b050fd75cc61
Breno <breno@MacBook-Pro-de-Ulisses.local>
2022-04-12 13:03:30 -0300

    Setup TS and Eslint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.status(200).json({ status: 'OK' }));
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).json({ status: 'OK Running' }));
 
 app.listen(4000, () => console.log('Running on folder 4000'));
